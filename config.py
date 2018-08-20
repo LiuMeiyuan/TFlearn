791188ad6090b641efeb54dbf219c2efb4e987fc
meiyuan liu <meiyuan.liu@istb.unibe.ch>
2018-08-20 18:14:18 +0200

    desparate

--- config.py
+++ config.py
@@ -2,11 +2,15 @@
 data_dir = 'data/ISIC-2017_Training_Data'
 gt_dir = 'data/ISIC-2017_Training_Part1_GroundTruth'
 model_save_path = 'output/'
+train_record_path = 'data/train.tfrecord'
+val_record_path = 'data/val.tfrecord'
+#im_shape = (1856, 2720, 3)
+im_shape = (928, 1344, 3)
 
 #train
-validation_fraction = 0.2
+val_fraction = 0.2
 epoch = 50
-batch_size = 1
+batch_size = 32 
 normalize = True
 learning_rate = 0.01
 
