33e88019c4da844adcb80a1993ac666cf51d76af
meiyuan liu <meiyuan.liu@istb.unibe.ch>
2018-08-24 16:09:53 +0200

    nope

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
 #files
-data_dir = 'data/ISIC-2017_Training_Data'
-gt_dir = 'data/ISIC-2017_Training_Part1_GroundTruth'
+data_dir = 'data/resize/ISIC-2017_Training_Data'
+gt_dir = 'data/resize/ISIC-2017_Training_Part1_GroundTruth'
 model_save_path = 'output/'
-train_record_path = 'data/train.tfrecord'
-val_record_path = 'data/val.tfrecord'
+#train_record_path = 'data/train.tfrecord'
+#val_record_path = 'data/val.tfrecord'
 #im_shape = (1856, 2720, 3)
-im_shape = (928, 1344, 3)
+#im_shape = (928, 1344, 3)
+im_shape = (448, 640, 3)
 
 #train
 val_fraction = 0.2
 epoch = 50
-batch_size = 32 
+batch_size = 8 
 normalize = True
-learning_rate = 0.01
+starter_learning_rate = 0.01
+decay_steps = 500
+decay_rate = 0.96
+train_steps = 200
+model_save_step = 1000
 
 #model
 depth = 4
